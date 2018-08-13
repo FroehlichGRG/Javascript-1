68424634ab67f984618ca0c0aa1c06ee30c33905
Xen <froehlich.grg@gmail.com>
2018-08-13 07:56:55 +0200

    Experiment upload

---
+++ jscript/shopitems.js
@@ -0,0 +1,36 @@
+var ShopItem = [];
+
+ShopItem[0] = {
+    id: "1",
+    name: "Машина",
+    urlthumb: "Lesson-6-img/car.jpg",
+    price: 2000
+};
+
+ShopItem[1] = {
+    id: "2",
+    name: "Башмаки",
+    urlthumb: "Lesson-6-img/shoes.jpg",
+    price: 800
+};
+
+ShopItem[2] = {
+    id: "3",
+    name: "Часы",
+    urlthumb: "Lesson-6-img/clock.jpg",
+    price: 1600
+};
+
+ShopItem[3] = {
+    id: "4",
+    name: "Мультитул",
+    urlthumb: "Lesson-6-img/multitool.jpg",
+    price: 100
+};
+
+ShopItem[4] = {
+    id: "4",
+    name: "WD 40",
+    urlthumb: "Lesson-6-img/wd40.jpg",
+    price: 5
+};
