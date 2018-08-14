70ba5f1b2ddbc14f9191760efc7d699a7595ccdc
Xen <froehlich.grg@gmail.com>
2018-08-14 15:45:58 +0200

    Experiment upload

--- jscript/shopitems.js
+++ jscript/shopitems.js
@@ -1,35 +1,35 @@
 var ShopItem = [];
 
 ShopItem[0] = {
-    id: "1",
+    ind: "0",
     name: "Машина",
     urlthumb: "Lesson-6-img/car.jpg",
     price: 2000
 };
 
 ShopItem[1] = {
-    id: "2",
+    ind: "1",
     name: "Башмаки",
     urlthumb: "Lesson-6-img/shoes.jpg",
     price: 800
 };
 
 ShopItem[2] = {
-    id: "3",
+    ind: "2",
     name: "Часы",
     urlthumb: "Lesson-6-img/clock.jpg",
     price: 1600
 };
 
 ShopItem[3] = {
-    id: "4",
+    ind: "3",
     name: "Мультитул",
     urlthumb: "Lesson-6-img/multitool.jpg",
     price: 100
 };
 
 ShopItem[4] = {
-    id: "4",
+    ind: "4",
     name: "WD 40",
     urlthumb: "Lesson-6-img/wd40.jpg",
     price: 5
